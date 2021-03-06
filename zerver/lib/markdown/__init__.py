ccffa175a12dc26bee5ad3833a92508395575840
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-03-06 14:56:32 +0000

    markdown: Increase priority of Tex pattern.
    This is a prep commit for the issue: #2401.
    The Tex pattern is assigned higher priority than Escape
    pattern so it doesn't break for cases like $$\{1, 2, 3\}$$.

--- zerver/lib/markdown/__init__.py
+++ zerver/lib/markdown/__init__.py
@@ -2208,12 +2208,12 @@ class Markdown(markdown.Markdown):
         reg = markdown.util.Registry()
         reg.register(BacktickInlineProcessor(markdown.inlinepatterns.BACKTICK_RE), "backtick", 105)
         reg.register(
+            Tex(r"\B(?<!\$)\$\$(?P<body>[^\n_$](\\\$|[^$\n])*)\$\$(?!\$)\B", self), "tex", 103
+        )
+        reg.register(
             markdown.inlinepatterns.DoubleTagPattern(STRONG_EM_RE, "strong,em"), "strong_em", 100
         )
         reg.register(UserMentionPattern(mention.MENTIONS_RE, self), "usermention", 95)
-        reg.register(
-            Tex(r"\B(?<!\$)\$\$(?P<body>[^\n_$](\\\$|[^$\n])*)\$\$(?!\$)\B", self), "tex", 90
-        )
         reg.register(StreamTopicPattern(get_compiled_stream_topic_link_regex(), self), "topic", 87)
         reg.register(StreamPattern(get_compiled_stream_link_regex(), self), "stream", 85)
         reg.register(Timestamp(r"<time:(?P<time>[^>]*?)>"), "timestamp", 75)
