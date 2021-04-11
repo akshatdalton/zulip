868330ede4f2fc6b5588b18b678f37189f50859c
Riken Shah <rikenshah.02@gmail.com>
2021-04-11 21:16:51 +0530

    devtools: Add markdown dev panel.
    This commit adds a new developer tool,
    "Markdown Developer Panel" which will help us:
    -> View markdown output from marked.js (frontend
    markdown).
    -> View markdown output from Python Markdown
    (backend markdown).
    -> View the difference between the above two results.
    
    Fixes #15021.

--- zproject/dev_urls.py
+++ zproject/dev_urls.py
@@ -18,6 +18,7 @@ from zerver.views.development.integrations import (
     get_fixtures,
     send_all_webhook_fixture_messages,
 )
+from zerver.views.development.markdown import markdown_panel
 from zerver.views.development.registration import (
     confirmation_key,
     register_development_realm,
@@ -79,6 +80,9 @@ urls = [
     path("devtools/integrations/<integration_name>/fixtures", get_fixtures),
     path("config-error/<error_category_name>", config_error, name="config_error"),
     path("config-error/remoteuser/<error_category_name>", config_error),
+    # Add a convenient way to test markdown with frontend (markdown.js) and
+    # backend (Python-Markdown) processor and compare their results.
+    path("devtools/markdown", markdown_panel),
     # Special endpoint to remove all the server-side caches.
     path("flush_caches", remove_caches),
 ]
